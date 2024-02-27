c6d5d44de881edd995fdc417c218a2d9205e1757
NSMNIA <kevin.stoop@revolveddesign.com>
2024-02-27 21:30:24 +0100

    feat(background): :sparkles: send test message
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

--- extension/background/index.ts
+++ extension/background/index.ts
@@ -1,1 +1,15 @@
+const NOTIFICATION = 'StandUpOrYoullTurnIntoABlobFish-NOTIFICATION';
 
+browser.alarms.create('', {
+  periodInMinutes: 0.1,
+});
+
+browser.alarms.onAlarm.addListener(() => {
+  browser.notifications.create(NOTIFICATION, {
+    type: 'basic',
+    iconUrl: browser.runtime.getURL('icons/icon.png'),
+    title: 'StandUpOrYoullTurnIntoABlobFish',
+    message: 'Test message',
+    imageUrl: browser.runtime.getURL('icons/icon.png'),
+  });
+});
